e9dea41b8d8f43801dfe73944268a0ce413b01f2
Felegyhazi David <felegyhazi.david@gmail.com>
2024-04-11 19:57:51 +0200

    new images

--- assets/images/Edina/images.ts
+++ assets/images/Edina/images.ts
@@ -18,8 +18,21 @@ import image16 from '@/assets/images/Edina/16.jpg';
 import image17 from '@/assets/images/Edina/17.jpg';
 import image18 from '@/assets/images/Edina/18.jpg';
 import image19 from '@/assets/images/Edina/19.jpg';
+import image20 from '@/assets/images/Edina/20.jpg';
+import image21 from '@/assets/images/Edina/21.jpg';
+import image22 from '@/assets/images/Edina/22.jpg';
+import image23 from '@/assets/images/Edina/23.jpg';
+import image24 from '@/assets/images/Edina/24.jpg';
+import image25 from '@/assets/images/Edina/25.jpg';
+//import image26 from '@/assets/images/Edina/26.jpg';
+import image27 from '@/assets/images/Edina/27.jpg';
+//import image28 from '@/assets/images/Edina/28.jpg';
+//import image29 from '@/assets/images/Edina/29.jpg';
+//import image30 from '@/assets/images/Edina/30.jpg';
 
 export const images = [image0, image1, image2, image3, image4,
                        image5, image6, image7, image8, image9,
                        image10, image11, image12, image13, image14,
-                       image15, image16, image17, image18, image19]
+                       image15, image16, image17, image18, image19,
+                       image20, image21, image22, image23, image24,
+                       image25, image27]
